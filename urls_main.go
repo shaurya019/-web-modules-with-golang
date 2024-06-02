29b908caa8f9862c5ebc0044b3d81dc4f728f89c
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-06-03 01:24:13 +0530

    url structure in golang

---
+++ urls_main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const myurl string = "https://lco.dev:3000/learn?coursename=reactjs&paymentid=ghbj456ghb"
+
+func main() {
+	fmt.Println("Welcome to handling URLs in golang")
+	fmt.Println(myurl)
+
+	res,_ := url.Parse(myurl);
+
+	fmt.Println(res.Scheme)
+	fmt.Println(res.Host)
+	fmt.Println(res.Path)
+	fmt.Println(res.Port())
+	fmt.Println(res.RawQuery)
+
+	qparams := res.Query();
+
+	fmt.Printf("The type of query params are: %T\n", qparams)
+
+	fmt.Println(qparams["coursename"])
+
+	for _,val := range qparams {
+		fmt.Println("Param is: ", val)
+	}
+
+	partsOfUrl := &url.URL{
+		Scheme:  "https",
+		Host:    "lco.dev",
+		Path:    "/tutcss",
+		RawPath: "user=Shaurya",
+	}
+
+	url := partsOfUrl.String();
+	fmt.Println(url);
+
+
+
+}
